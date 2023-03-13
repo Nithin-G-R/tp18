dfc10daac627a7002ccf1b7cd3d1c1fe81b105a8
nithin <nithinrangaraj@gmail.com>
2023-03-13 21:26:42 +1100

    first commit

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template
+from flask import request
+import sqlite3
+from util import base64_to_pil
+from werkzeug.utils import secure_filename
+
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+import cv2
+from tensorflow import keras
+
+import model
+import os
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+
+mod = keras.models.load_model('waste_mobile_without.h5')
+
+@app.route('/home')
+@app.route('/')
+def HomePage():  # put application's code here
+    return model.HomePage()
+
+
+@app.route('/InfoHome')
+@app.route('/InfoHome/<int:section_id>', methods=['GET'])
+def InfoPage(section_id=0):
+    # section_id = request.values.get("section_id")
+    if section_id == 0:
+        path = "impact.html"
+    elif section_id == 1:
+        path = "reuse.html"
+    elif section_id == 2:
+        path = "analysis.html"
+    else:
+        path = None
+
+        # if path:
+    last_dot_index = path.rfind(".")
+    title = path[:last_dot_index]
+    with open("templates/InfoPages/%s" % path, "r") as f:
+        content = f.read()
+
+    return model.InfoPage(section_id, title=title, content=content)
+
+# AI model prediction
+def predict_cls(img):
+    classes = ['ORGANIC', 'RECYCLABLE']
+    SIZE = 128
+
+    final_img = np.expand_dims(cv2.resize(img, (SIZE, SIZE)), axis=0)
+    probs = mod(final_img)
+    return classes[np.argmax(probs)]
+
+@app.route('/sorting', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+
+        img = Image.open(f)
+        np_img = np.array(img)
+
+        cls = predict_cls(np_img)
+        
+        print(request.files)
+        f.save(secure_filename(f.filename))
+
+        return model.resultPage(cls)
+    else:
+        return model.sortingPage()
+
+@app.route('/about')
+def aboutpage():
+    return model.AboutPage()
+
+
+@app.route('/map')
+def mappage():
+    return model.MapPage()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,38 @@
+import view
+from flask import render_template
+
+webpage_view = view.View()
+
+
+def HomePage():
+    return webpage_view("HomePage")
+
+
+def InfoPage(section=None, title=None, content=None):
+    base_menus = [
+        ["impact", 0, "/InfoHome/0"],
+        ["Reuse Methods", 0, "/InfoHome/1"],
+        ["analysis", 0, "/InfoHome/2"],
+    ]
+
+    menus = base_menus.copy()
+    if section:
+        menus[section][1] = "active"
+    else:
+        menus[0][1] = "active"
+
+    return webpage_view("InfoHome", section=section, title=title, content=content, menus=menus)
+
+
+
+def sortingPage():
+    return webpage_view("SortingPage")
+
+def AboutPage():
+    return webpage_view('about')
+
+def MapPage():
+    return webpage_view('Map')
+
+def resultPage(cls=None):
+    return webpage_view("result", cls=cls)
