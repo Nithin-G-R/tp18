17806f0b4159fb7199a3ea5096f4cf4bcf8c2084
Nithin-G-R <127166295+Nithin-G-R@users.noreply.github.com>
2023-04-23 12:59:09 +1000

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import tensorflow as tf
 import cv2
 from tensorflow import keras
 
+import torch
 from transformers import LongformerTokenizer, EncoderDecoderModel
 
 model1 = EncoderDecoderModel.from_pretrained("patrickvonplaten/longformer2roberta-cnn_dailymail-fp16")
